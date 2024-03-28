8f97988510ecd11e2659c902d1f948cea9871f9b
Chris7654 <158368448+Chris7654@users.noreply.github.com>
2024-03-28 18:27:18 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,18 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    loader: 'jsx',
+  },
   build: {
     chunkSizeWarningLimit: 2000, // handle warning on vendor.js bundle size
   },
   base: "/yolov5-tfjs/",
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
 });
